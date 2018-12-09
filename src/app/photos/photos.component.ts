9778901de9814e42e740b8b681940b426d95faf3
PrashantC1990 <maddict1@gmail.com>
2018-12-10 03:13:16 +0530

    first project

---
+++ src/app/photos/photos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html',
+  styleUrls: ['./photos.component.css']
+})
+export class PhotosComponent implements OnInit {
+
+  photos$: Object;
+
+  constructor(private data:DataService) { }
+
+  ngOnInit() {
+    this.data.getPhotos().subscribe(
+      data => this.photos$ =data
+    )
+  }
+
+}
