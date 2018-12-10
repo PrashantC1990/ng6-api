d2a266306b5d6a3765ebfe79a643bd6c5626aee0
PrashantC1990 <maddict1@gmail.com>
2018-12-10 14:55:59 +0530

    https

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -9,18 +9,18 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getUsers(){
-    return this.http.get('http://jsonplaceholder.typicode.com/users')
+    return this.http.get('https://jsonplaceholder.typicode.com/users')
   }
 
   getUser(userId){
-    return this.http.get('http://jsonplaceholder.typicode.com/users/'+userId)
+    return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
   }
 
   getPosts(){
-    return this.http.get('http://jsonplaceholder.typicode.com/posts')
+    return this.http.get('https://jsonplaceholder.typicode.com/posts')
   }
 
   getPhotos(){
-    return this.http.get('http://jsonplaceholder.typicode.com/photos')
+    return this.http.get('https://jsonplaceholder.typicode.com/photos')
   }
 }
